e4678a645dab3cfbf56f999dc3a2d3339521c715
itkncoder <kncoderr@gmail.com>
2024-08-18 10:31:57 +0500

    change

---
+++ src/pages/home.jsx
@@ -0,0 +1,16 @@
+import Layout from "../layout/layout"
+import Wrapper from "../layout/wrapper"
+
+const Home = () => {
+    return (
+        <div>
+            <Layout>
+                <Wrapper>
+                    sdfsdfsdf
+                </Wrapper>
+            </Layout>
+        </div>
+    )
+}
+
+export default Home
