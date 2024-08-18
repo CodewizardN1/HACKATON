3dcff2f357d2a2fa95480db2b54251237548e97e
itkncoder <kncoderr@gmail.com>
2024-08-18 15:41:53 +0500

    f

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { Route, Routes } from "react-router-dom"
 import Home from "./pages/home"
 import './index.css'
+import Add from "./pages/add"
 const App = () => {
     return (
         <div className="bg-[#F5F5F5] min-h-screen">
             <Routes>
                 <Route path="/" element={<Home/>} />
+                <Route path="/add" element={<Add/>} />
             </Routes>
         </div>
     )
